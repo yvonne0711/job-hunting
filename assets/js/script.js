481b9dfbdf42b8ee7ce7b61c02fe5c7b7ed750dd
yvonne <yvonnewong1107@gmail.com>
2023-11-30 19:54:38 +0000

    Toggled left sidebar with no added features. Used mapbox gl js for this

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,22 @@ const map = new mapboxgl.Map({
     projection: 'naturalEarth', // starting projection
     maxBounds: bounds,
     });
+
+    function toggleSidebar(id) {
+        const elem = document.getElementById(id);
+        // Add or remove the 'collapsed' CSS class from the sidebar element.
+        // Returns boolean "true" or "false" whether 'collapsed' is in the class list.
+        const collapsed = elem.classList.toggle('collapsed');
+        const padding = {};
+        // 'id' is 'right' or 'left'. When run at start, this object looks like: '{left: 300}';
+        padding[id] = collapsed ? 0 : 300; // 0 if collapsed, 300 px if not. This matches the width of the sidebars in the .sidebar CSS class.
+        // Use `map.easeTo()` with a padding option to adjust the map's center accounting for the position of sidebars.
+        map.easeTo({
+        padding: padding,
+        duration: 1000 // In ms. This matches the CSS transition duration property.
+        });
+        }
+         
+        map.on('load', () => {
+        toggleSidebar('left');
+        });
