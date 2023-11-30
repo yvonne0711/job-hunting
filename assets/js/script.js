c989ac76278a711a009487445787601d55c79049
AliochinM <aliochinm@gmail.com>
2023-11-30 19:08:43 +0000

    adds a search function, that's limited to the uk, can search for cities or countys, will zoom in on the spot searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,31 @@ fetch("https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id="+adzunaApiID+"&app
 })
 
 
-//Mapbox api code, Note for coordinates it's always [long, lat]
+
+//Mapbox api code, Note for coordinates it's always [long, lat] unless stated otherwise
 mapboxgl.accessToken = mapboxApiKey;
 const bounds = [
     [-11.597796819294665, 49.6674427790888], // Southwest coordinates
     [1.6745909020453322, 60.88683897281768] // Northeast coordinates
     ];
 
+//Renders map 
 const map = new mapboxgl.Map({
     container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    // Choose from Mapbox's core styles [https://docs.mapbox.com/api/maps/styles/]
     style: 'mapbox://styles/mapbox/navigation-night-v1',
     center: [-2.5420163811789536, 54.93314456712494],
     zoom: 7,
-    projection: 'naturalEarth', // starting projection
     maxBounds: bounds,
     });
+
+   
+//search functionality
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        countries: 'gb', //limits search to the UK
+        mapboxgl: mapboxgl
+    }
+    ));
+
