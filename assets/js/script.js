750fd7ea8b2cf41e6b5700f83902e1dcc69badf0
yvonne <yvonnewong1107@gmail.com>
2023-12-01 18:12:07 +0000

    keep mapbox's working left sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,14 @@ const map = new mapboxgl.Map({
         map.on('load', () => {
         toggleSidebar('left');
         });
+
+        // to work on
+    // function openNav() {
+    //     document.getElementById("mySidebar").style.width = "250px";
+    //     document.getElementById("main").style.marginLeft = "250px";
+    //   }
+      
+    //   function closeNav() {
+    //     document.getElementById("mySidebar").style.width = "0";
+    //     document.getElementById("main").style.marginLeft= "0";
+    //   }
